ff291df9a2f8c2b9a4e3bf17196ad1ee4f4ed693
rknLA <kevin@rkn.la>
2012-11-19 23:04:53 -0800

    Check for headers and accept in init.

--- libcrest/Session.py
+++ libcrest/Session.py
@@ -1,6 +1,14 @@
+from types import *
+
 class CrestSession:
   def __init__(self, url, accept='application/json', headers=None, data=None):
-    self.accept = accept
     self.base_url = url
-    self.default_headers = headers if headers else []
-    self.default_data = data if data else {}
+    self.default = { }
+    self.default['accept'] = accept
+    if type(headers) == StringType:
+      self.default['headers'] = [headers]
+    elif type(headers) == ListType:
+      self.default['headers'] = headers
+    else:
+      self.default['headers'] = []
+    self.default['data'] = data if data else {}
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -6,7 +6,22 @@ class TestCrestInitialization(unittest.TestCase):
   def test_initialization_with_url_only(self):
     instance = CrestSession('api.github.com')
     self.assertEqual(instance.base_url, 'api.github.com')
+    # test defaults
+    self.assertEqual(instance.default['accept'], 'application/json')
+    self.assertEqual(instance.default['headers'], [])
+    self.assertEqual(instance.default['data'], {})
 
   def test_initialization_without_url(self):
     with self.assertRaises(TypeError):
       instance = CrestSession() # this should fail.
+
+  def test_initialize_with_accept(self):
+    instance = CrestSession('api.github.com', 'application/xml')
+    self.assertEqual(instance.default['accept'], 'application/xml')
+
+  def test_initialize_with_headers_string(self):
+    instance = CrestSession('api.github.com',
+                            headers="Content-Type: application/json")
+    self.assertListEqual(instance.default['headers'],
+                         [ "Content-Type: application/json" ])
+
