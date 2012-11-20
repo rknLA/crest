d0eed102929418bb70bec43a820489e6765b5741
rknLA <kevin@rkn.la>
2012-11-19 22:28:30 -0800

    Import crest in test.
    Test fails.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,10 +1,12 @@
 import unittest
-import libcrest as crest
+from libcrest import Crest
 
 class TestCrestInitialization(unittest.TestCase):
 
-  def setUp(self):
-    print "just settin' up"
+  def test_initialization_with_url_only(self):
+    instance = Crest('api.github.com')
+    self.assertEqual(instance.base_url, 'api.github.com')
 
-  def test_fail(self):
+  def test_initialization_without_url(self):
+    instance = Crest() # this should fail.
     self.assertTrue(False)
