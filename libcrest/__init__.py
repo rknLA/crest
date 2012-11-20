dbc0d5e0c24ea62e3542c1e04758f75b28c63bb6
rknLA <kevin@rkn.la>
2012-11-19 22:52:23 -0800

    Green second test. Restructure.

--- libcrest/__init__.py
+++
@@ -1,8 +0,0 @@
-
-class Crest:
-  def __init__(self, url, accept='application/json', headers=None, data=None):
-    self.accept = accept
-    self.base_url = url
-    self.default_headers = headers if headers else []
-    self.default_data = data if data else {}
-
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,12 +1,12 @@
 import unittest
-from libcrest import Crest
+from libcrest.Session import CrestSession
 
 class TestCrestInitialization(unittest.TestCase):
 
   def test_initialization_with_url_only(self):
-    instance = Crest('api.github.com')
+    instance = CrestSession('api.github.com')
     self.assertEqual(instance.base_url, 'api.github.com')
 
   def test_initialization_without_url(self):
-    instance = Crest() # this should fail.
-    self.assertTrue(False)
+    with self.assertRaises(TypeError):
+      instance = CrestSession() # this should fail.
